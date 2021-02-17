f216182e2279002b731e0902d41155403aadfed8
KelsiAnderson <kelsi.anderson26@gmail.com>
2021-02-17 11:32:04 -0700

    changed pages file to component

--- components/app.jsx
+++ components/app.jsx
@@ -1,4 +1,23 @@
 // This will be the app component to run the app 
+// "use strict";
+// class App extends React.Component {
+//     constructor(props){
+//         super(props);
+//     } render() {
+//     return (
+//         <Router>
+//         <div><NavComponent/></div>
+//             <Switch>
+//                 <Route exact path='/about'>
+//                 <About/>
+//                 </Route>
+//             </Switch>
+//         </Router>
+//     );
+//     }
+// }
+// React.DOM.render(<Aap/>,document.getElementById('app'))
+// This will be the app component to run the app 
 "use strict";
 
 function App(){
