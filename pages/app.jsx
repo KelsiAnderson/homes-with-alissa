960637477d47482495429bf5a15b3b66720041c4
KelsiAnderson <kelsi.anderson26@gmail.com>
2021-02-18 10:36:40 -0700

    working on dato file and updated code on app.jsx to work better with graphQl and dato

--- pages/app.jsx
+++ pages/app.jsx
@@ -19,10 +19,32 @@
 // React.DOM.render(<Aap/>,document.getElementById('app'))
 // This will be the app component to run the app 
 "use strict";
+import { GraphQLClient, ClientContext } from 'graphql-hooks';
+import { useQuery } from "graphql-hooks";
+const client = new GraphQLClient({
+  url: "https://graphql.datocms.com/",
+  headers: {
+    "Authorization": "Bearer a5419b93416fd17b98d19bf08bbe49",
+  }
+});
+
+const HOMEPAGE_QUERY = `query MyQuery {
+    listing {
+      title
+    }
+  }`;
 
 function App(){
+    const { loading, error, data } = useQuery(HOMEPAGE_QUERY, {
+        variables: {
+          limit: 10
+        }
+      });
+      if (loading) return "Loading...";
+      if (error) return "Something Bad Happened";
     return (
         <Router>
+        {JSON.stringify(data)}
         <div><NavComponent/></div>
             <Switch>
                 <Route exact path='/about'>
@@ -32,6 +54,18 @@ function App(){
                 </Route>
             </Switch>
         </Router>
+
     );
 }
-React.DOM.render(<Aap/>,document.getElementById('app'))
+// React.DOM.render(<App/>,document.getElementById('app'))
+ReactDOM.render(
+    <React.StrictMode>
+      <ClientContext.Provider value={client}>
+        <App />
+      </ClientContext.Provider>
+    </React.StrictMode>,
+    document.getElementById("app")
+  );
+
+  export default App
+  
